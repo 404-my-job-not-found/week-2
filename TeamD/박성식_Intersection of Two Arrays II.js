76258a0580b8261c9a5551725d32da7f798a0a5d
ssp <qkrtjdtlr02@naver.com>
2025-06-26 23:20:28 +0900

    intersection two array 3번째 시도

--- TeamD/박성식_Intersection of Two Arrays II.js
+++ TeamD/박성식_Intersection of Two Arrays II.js
@@ -1,9 +1,11 @@
 const intersect = (nums1, nums2) => {
-  return nums1.filter((num) => nums2.includes(num));
+  return nums1
+    .filter((num) => nums2.includes(num))
+    .splice(0, Math.min(nums1.length, nums2.length));
 };
 
 console.log(intersect([1, 2, 2, 1], [2, 2])); // [2, 2]
 console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4])); // [4, 9]
 console.log(intersect([1], [1])); // [1]
 console.log(intersect([3, 1, 2], [1, 1])); // [1]
-console.log(intersect([1, 2, 2, 1], [2])); // [2, 2]
+console.log(intersect([1, 2, 2, 1], [2])); // [2]
