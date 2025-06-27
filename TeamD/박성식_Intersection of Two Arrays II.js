fe049f17700d14bb08caec4b46763cb8a7c596cf
ssp <qkrtjdtlr02@naver.com>
2025-06-27 22:59:56 +0900

    Intersection of Two Arrays II

--- TeamD/박성식_Intersection of Two Arrays II.js
+++ TeamD/박성식_Intersection of Two Arrays II.js
@@ -1,7 +1,24 @@
 const intersect = (nums1, nums2) => {
-  return nums1
-    .filter((num) => nums2.includes(num))
-    .splice(0, Math.min(nums1.length, nums2.length));
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] === nums2[j]) {
+      result.push(nums1[i]);
+      i++;
+      j++;
+    } else if (nums1[i] < nums2[j]) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+
+  return result;
 };
 
 console.log(intersect([1, 2, 2, 1], [2, 2])); // [2, 2]
