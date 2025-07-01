e6c3b0f3da7bd60ce41bd3dd8db4fbb6e67a9607
stoic-park <demige79@gmail.com>
2025-07-01 22:10:42 +0900

    solve: longest substring 2

--- TeamA/박성택_Longest Substring Without Repeating Characters.js
+++ TeamA/박성택_Longest Substring Without Repeating Characters.js
@@ -10,19 +10,43 @@ var lengthOfLongestSubstring = function(s) {
     // 있으면, targetArr.length push, targetArr 초기화, push i 
     // 나중에 math.max
 
-    let targetArr = [];
-    const lengthArr = [];
-    for(let i=0; i<s.length; i++) {
-        if(targetArr.includes(s[i])) {
-            lengthArr.push(targetArr.length);
-            targetArr = [];
-            targetArr.push(s[i]);
-        }
-        else {
-            targetArr.push(s[i]);
-        }
-    }
+    // let targetArr = [];
+    // const lengthArr = [];
+    // for(let i=0; i<s.length; i++) {
+    //     if(targetArr.includes(s[i])) {
+    //         lengthArr.push(targetArr.length);
+    //         targetArr = [];
+    //         targetArr.push(s[i]);
+    //     }
+    //     else {
+    //         targetArr.push(s[i]);
+    //     }
+    // }
 
 
-    return Math.max(...lengthArr, 0);
+    // return Math.max(...lengthArr, 0);
+
+    let maxLength = 0;
+    let left = 0;
+
+    const seen = new Set();
+
+    for(let right = 0; right < s.length; right++) {
+        // 현재 문자
+        const currentChar = s[right];
+
+        // 현재 문자가 이미 있으면, 왼쪽 포인터를 오른쪽으로 한 칸 이동
+        while(seen.has(currentChar)) {
+            seen.delete(s[left]);
+            left++;
+        }
+
+        // 현재 문자를 추가
+        seen.add(currentChar);
+
+        // 최대 길이 갱신
+        maxLength = Math.max(maxLength, right - left + 1); // 현재 문자열의 길이와 비교해서 갱신
+    }   
+
+    return maxLength;
 };
